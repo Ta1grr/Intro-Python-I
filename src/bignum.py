c810660c8f6dfa9f3dab2aa6416c34c36086cd3b
Ta1grr <taile602@gmail.com>
2019-02-12 18:09:52 -0600

    completed datatypes.py and added in comments for bignum.py

--- src/bignum.py
+++ src/bignum.py
@@ -2,5 +2,9 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+# printing out an integer 2
 print(2)
+# 2 to the 65536 power
 print(pow(2, 65536))
+# 2 to the 65536 power as well.
+print(2 ** 65536)
